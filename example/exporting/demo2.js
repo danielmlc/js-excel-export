bd4054d5843ed52538ee7f40cda051c88dce69b9
danielmlc <danielmlc@126.com>
2019-10-10 19:29:13 +0800

    更新

--- example/exporting/demo2.js
+++ example/exporting/demo2.js
@@ -2,20 +2,23 @@
  * @Description: 未描述
  * @Author: danielmlc
  * @Date: 2019-10-08 16:59:20
- * @LastEditTime: 2019-10-09 12:32:21
+ * @LastEditTime: 2019-10-10 19:10:34
  */
 const ExcelCls = require('../../lib/xlsx.js')
 const configJson = require('../data/data1.structure.json')
-const testData = require('../data/data1.json')
+const testData = require('../data/simple.data.json')
 let _excel = new  ExcelCls(
     {
         workbook:{
-            creator: 'xxxxx',
+            creator: '租户管理员',
         }
     }  
 )
-let workbook = _excel.getWorkbook();
-let worksheet = _excel.getSheet(workbook,configJson.sheetConf,testData);
-worksheet.commit()
-workbook.commit() 
+_excel.getWorkbook(null,function(wb){
+    let sheetConf = configJson.sheetConf;
+     _excel.getSheet(wb, sheetConf.title,function(ws){
+        _excel.addSimpleTable(ws, sheetConf.header,testData);
+     });
+});
+
 console.log('done!')
